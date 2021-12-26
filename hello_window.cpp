d3953a7d0b109cfa61a50f980d12f44c759afad2
Surelye <sgnoteii55@gmail.com>
2021-12-27 00:10:48 +0400

    now shaders stored in extra files

--- hello_window.cpp
+++ hello_window.cpp
@@ -15,16 +15,102 @@
 #include <iostream>
 
 #include <GL/glew.h> // Подключаем glew для того, чтобы получить все необходимые заголовочные файлы
-// ДАВАЙТЕ БУДЕМ МОЛОДЦАМИ И БУДЕМ ИСПОЛЬЗОВАТЬ ДИРЕКТИВЫ ifndef и define ... 
+
 class Shader
 {
 	public:
 	// Идентификатор программы
 	GLuint Program;
 	// Конструктор считывает и собирает шейдер
-	Shader(const GLchar * vertexPath, const GLchar * fragmentPath);
+	// Shader(const GLchar * vertexPath, const GLchar * fragmentPath);
 	// Использование программы
-	void Use();
+	// void Use();
+
+	// Считывание файла шейдера. Для считывания используем стандартные потоки C++, помещая
+	// результат в строки.
+	Shader(const GLchar * vertexPath, const GLchar * fragmentPath)
+	{
+		// 1. Получаем исходный код шейдера из filePath
+		std::string vertexCode;
+		std::string fragmentCode;
+		std::ifstream vShaderFile;
+		std::ifstream fShaderFile;
+
+		// Удостоверимся, что ifstream объекты могут выкидывать исключения
+		vShaderFile.exceptions(std::ifstream::badbit);
+		fShaderFile.exceptions(std::ifstream::badbit);
+
+		try
+		{
+			// Открываем файлы
+			vShaderFile.open(vertexPath);
+			fShaderFile.open(fragmentPath);
+			std::stringstream vShaderStream, fShaderStream;
+			// Считываем данные в потоки
+			vShaderStream << vShaderFile.rdbuf();
+			fShaderStream << fShaderFile.rdbuf();
+			// Закрываем файлы
+			vShaderFile.close();
+			fShaderFile.close();
+			// Преобразовываем потоки в массив GLchar
+			vertexCode = vShaderStream.str();
+			fragmentCode = fShaderStream.str();
+		}
+		catch(std::ifstream::failure e)
+		{
+			std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+		}
+		const GLchar * vShaderCode = vertexCode.c_str();
+		const GLchar * fShaderCode = fragmentCode.c_str();
+
+		// Теперь нужно скомпилировать и слинковать шейдер.
+		// 2. Сборка шейдеров
+		GLuint vertex, fragment;
+		GLint success;
+		GLchar infoLog[512];
+
+		// Вершинный шейдер
+		vertex = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertex, 1, &vShaderCode, NULL);
+		glCompileShader(vertex);
+		// Если есть ошибки, то вывести их
+		glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+
+		if (!success)
+		{
+			glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		}
+
+		fragment = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragment, 1, &fShaderCode, NULL);
+		glCompileShader(fragment);
+		glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+
+		if (!success)
+		{
+			glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+		}
+
+		// Шейдерная программа 
+		this->Program = glCreateProgram();
+		glAttachShader(this->Program, vertex);
+		glAttachShader(this->Program, fragment);
+		glLinkProgram(this->Program);
+		
+		// Если есть ошибки, то вывести их
+		glGetProgramiv(this->Program, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			glGetProgramInfoLog(this->Program, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+		}
+		glDeleteShader(vertex);
+		glDeleteShader(fragment);
+	}
+
+	void Use() { glUseProgram(this->Program); }
 };
 
 #endif
@@ -436,6 +522,9 @@ int main()
 	// шейдере. Получив значение индекса атрибута можно вместить туда необходимые данные. Для 
 	// демонстрации работы этой функции будет менять цвет от времени (реализация в игровом цикле).
 
+	Shader ourShader("/home/surelye/Desktop/repos/OpenGL/vertex_shader.vs", 
+					 "/home/surelye/Desktop/repos/OpenGL/fragment_shader.frag");
+
 	// Игровой цикл.
 	while (!glfwWindowShouldClose(window))
 	{
@@ -448,7 +537,8 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// Активируем шейдерную программу
-		glUseProgram(shaderProgram);
+		// glUseProgram(shaderProgram);
+		ourShader.Use();
 
 		// Обновляем цвет формы
 		// GLfloat timeValue = glfwGetTime();
